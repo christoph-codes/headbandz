b0811f16483986a7b3232787482a376914b745ac
Christopher Jones <tkcwebdev@gmail.com>
2021-12-17 00:48:05 -0800

    main: Added footer for layout purposes WIP

---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,8 @@
+<style lang="scss">
+    .Footer {
+        background-color: var(--secondary-dark);
+        padding: 0.5rem;
+    }
+</style>
+
+<footer class="Footer">Footer</footer>
--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import Header from '../components/Header.svelte';
+    import Footer from '../components/Footer.svelte';
 </script>
 
-<Header></Header>
+<Header/>
+<slot/>
+<Footer/>
